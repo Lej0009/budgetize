4635096260d98f8612b982a590c1a92d7af1803d
Lauren Jahera <lauren.jahera@gmail.com>
2019-11-21 06:39:39 -0500

    updated wireframe

--- src/main/java/co/budgetize/budgetize/controllers/ExpenseController.java
+++ src/main/java/co/budgetize/budgetize/controllers/ExpenseController.java
@@ -28,6 +28,9 @@ public class ExpenseController {
         return "expense/welcome";
     }
 
+    @RequestMapping(value = "welcome")
+    public String welcome2(Model model) { return "expense/welcome";}
+
     @RequestMapping(value = "login")
     public String login(Model model) {
         return "expense/login";
@@ -38,28 +41,38 @@ public class ExpenseController {
         return "expense/register";
     }
 
-    // Request path: budgetize/expense
     @RequestMapping(value = "index")
     public String index(Model model) {
 
+        //if user in session:
         model.addAttribute("expenses", expenseDao.findAll());
         model.addAttribute("title", "My Expenses");
 
         return "expense/index";
+
+        // else:
+        // return "login";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddExpenseForm(Model model) {
+
+        //if user in session:
         model.addAttribute("title", "Add Expense");
         model.addAttribute(new Expense());
 
         return "expense/add";
+
+        //else:
+        //return "login";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String processAddExpenseForm(@ModelAttribute @Valid Expense newExpense,
                                         Errors errors, Model model) {
 
+
+        //if user in session:
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Expense");
             return "expense/add";
@@ -75,42 +88,60 @@ public class ExpenseController {
 //        newExpense.setAmount(amount);
         expenseDao.save(newExpense);
         return "redirect:";
+
+        //else:
+        //return "login";
     }
 
     @RequestMapping(value = "delete", method = RequestMethod.GET)
     public String displayRemoveExpenseForm(Model model) {
+
+        //if user in session:
         model.addAttribute("expenses", expenseDao.findAll());
         model.addAttribute("title", "Remove Expense");
         return "expense/delete";
+
+        //else:
+        //return "login";
     }
 
     @RequestMapping(value = "delete", method = RequestMethod.POST)
     public String processRemoveExpenseForm(@RequestParam int[] expenseIds) {
 
+        //if user in session:
         for (int expenseId : expenseIds) {
             expenseDao.delete(expenseId);
         }
 
         return "redirect:";
+
+        //else:
+        //return "login";
     }
 
     @RequestMapping(value = "bymonth")
     public String byMonth(Model model) {
 
-
-
+        //if user in session:
         model.addAttribute("expenses", expenseDao.findAll());
         model.addAttribute("title", "Expenses by Month");
 
         return "expense/bymonth";
+
+        //else:
+        //return "login";
     }
 
     @RequestMapping(value = "bycategory")
     public String byCategory(Model model) {
 
+        //if user in session:
         model.addAttribute("expenses", expenseDao.findAll());
         model.addAttribute("title", "Expenses by Category");
 
         return "expense/bycategory";
+
+        //else:
+        //return "login";
     }
 }

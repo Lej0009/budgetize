2636a324ad1959c4d3b7bb77a87e92ebf6d3e5ee
Lauren Jahera <lauren.jahera@gmail.com>
2020-01-19 12:10:09 -0500

    connect ClearDB to MySQL

--- src/main/java/co/budgetize/budgetize/controllers/MainController.java
+++ src/main/java/co/budgetize/budgetize/controllers/MainController.java
@@ -5,6 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Controller
 @RequestMapping("/")
 public class MainController implements WebMvcConfigurer {
@@ -12,5 +18,15 @@ public class MainController implements WebMvcConfigurer {
     @RequestMapping(value = "")
     public String welcome(Model model) {
         return "welcome";
+    }
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
+
+        return DriverManager.getConnection(dbUrl, username, password);
     }
 }
